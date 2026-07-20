10fa6ae3610850d43abfebd8ce21054c881f40cd
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2026-07-20 19:36:32 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("rodou")
+
+//operações matematicas
+// + - * /
+const numero1 = 10
+const numero2 = 5
+//decimais representados pelo "."
+const resultado = numero1 / (numero2 + 2.5)
+console.log(resultado, resultado, 'abacaxi resultado')
+
+const nome = "Anderson"
+const sobrenome = "Keller"
+const saudacao = "Olá, meu nome completo é:"
+const nomeCompleto = saudacao + " " + nome + " " + sobrenome
+//template string
+const templateString = `Olá, meu nome completo é: ${nome} ${sobrenome}`
+
+
+console.log(nomeCompleto)
+console.log(templateString)
+//operadores % "mod"
+const numero = 15
+// console.log(numero % 5)
+//escrever o nome completo
+
+console.log("meu nome é Anderson Keller") //errado
+const nome = "Anderson"
+const sobrenome = "Keller"
+console.log(nome + " "+sobrenome)
+
+
+// const numeroBom =
+
+
+console.log("esse numero é ",numeroBom)
+
+
