92f567ff1eb707a511eec9096e6e3c28cea06801
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2026-07-21 19:47:03 -0300

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,33 @@
+console.log("script 3")
+
+ 
+const nome = "anderson keller "
+const nomeSemEspaco = nome
+// console.log(nome.length,'tamanho da palavra')
+
+let teste = ""
+teste += "teste"
+// console.log(teste,'variavel teste')
+// teste += "aumentando"
+// console.log(teste,'variavel teste')
+
+// let numero = 0
+// console.log(numero,'numero')
+// numero += 1
+// console.log(numero,'numero')
+// numero += 1
+// console.log(numero,'numero')
+
+const palavra = "Anderson"
+// A n d e r s o n
+// 0 1 2 3 4 5 6 7 = index ou indice
+console.log(palavra[2],'palavra no index 2, ou posição 2')
+let palavrainvertida = ""
+for(let i=0; i < palavra.length; i++){
+    console.log("for",i, palavra[i])
+    
+}
+
+
+
+
