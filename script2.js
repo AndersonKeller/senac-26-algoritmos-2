92fc32930f86e5de47cae21b2e5855dfa723c530
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2026-07-20 21:24:12 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,25 @@
+
+// const numero = prompt("digite um numero")
+
+
+// if(numero % 2 == 0){
+//     alert(numero + ' numero par')
+    
+// }else{
+//     alert(numero+' numero impar')
+    
+// }
+//alert(`numero é ${ numero % 2 == 0 ?'numero par': numero%5==0?'multiplo de 5':'numero impar'}`)
+// const anoDigitado = prompt("Digite o ano que voce nasceu")
+// //parseInt para converter o texto para numero
+// const ano = parseInt(anoDigitado)
+// alert(`Sua idade é de ${2026 - ano}`)
+
+const nome = prompt("Digite seu nome")
+// const numero = 10
+alert('seu nome tem '+nome.length+" letras")
+
+
+
+
+
