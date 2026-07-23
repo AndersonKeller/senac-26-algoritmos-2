1e34dad95f207a695be81d404ae73fa1a4423d1c
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2026-07-22 21:52:39 -0300

    Add files via upload

---
+++ script5.js
@@ -0,0 +1,62 @@
+// console.log("script5")
+
+
+// const listas = [
+//     [1,2,3],
+//     [2,4,5],
+//     [9,0,1],
+//     [5,5,6]
+// ]
+// console.log(listas,'listas')
+// for(let i = 0; i<listas.length; i++){
+//     console.log(listas[i],'listas[i]')
+//     // 0 1 2 
+//     for(let j=0; j < listas[i].length;j++){
+//         console.log(listas[i][j],'listas no for interno')
+//         console.log(listas[j],'na j')
+//         // listas[j].push("teste")
+//     }
+// }
+// console.log(listas,'listas final')
+
+// const produtosgerais = []
+// const produtosjulho = ['tv','sofá','colchao']
+
+// produtosgerais.push(produtosjulho)
+// console.log(produtosgerais,'gerais')
+// const produtosagosto = ['maquina de lavar']
+// produtosgerais.push(produtosagosto)
+// console.log(produtosgerais,'gerais 2')
+
+
+//produtos
+
+const produtos = [
+    {
+        name:"Tv",
+        preco:3000,
+        fotos:[
+            "url",
+            'url',
+            'url'
+        ]
+    },
+    {
+        name:"Aspirador de pó",
+        preco:300,
+        fotos:[
+            "url"
+        ]
+    },
+
+]
+for(let i = 0; i<produtos.length;i++){
+    
+    //monta o html
+    for(let j = 0; j<produtos[i].fotos.length;j++){
+        //monta o html das fotos
+    }
+    // .fotos
+}
+
+
